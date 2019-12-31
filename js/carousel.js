af9e674df1a562129b9ffb8f730e909e9c82772b
Joe MJ <44471182+Jackpock123@users.noreply.github.com>
2019-12-31 13:13:52 +0000

    Update to JavaScript files and travel webpages
    A new FAC and Travel folder and also JS updates.

--- js/carousel.js
+++ js/carousel.js
@@ -1,10 +1,10 @@
-// 2) Image Carousel
-// F&C: 
+// STEP 1: Define functions that control slide index number
 
 var slideIndex = 1
 
 //Next/previous controls
 plusSlides = function(n) {
+	//clearTimeout(showSlides);
 	showSlides(slideIndex += n);
 	// console.log(n);
 	// console.log(slideIndex);
@@ -12,12 +12,17 @@ plusSlides = function(n) {
 
 // Thumnail image control
 currentSlide = function(n) {
+	//clearTimeout(showSlides);
 	showSlides(slideIndex = n);
 	// console.log(n);
 	// console.log(slideIndex);
 }
 
+
+// STEP 2: Define function which displays slide depending on slide index
+
 showSlides = function(n) {
+	
 	// alert('Hello');
 	var i;
 	var slides = document.getElementsByClassName("js-mySlides");
@@ -38,15 +43,114 @@ showSlides = function(n) {
 	}
 	slides[slideIndex-1].style.display = "block";
   	dots[slideIndex-1].className += " active";
-  	//console.log(slideIndex);
-
-  	setTimeout(function() {
-		plusSlides(1);
-	}, 10000);
+  	//console.log(slideIndex);  	
 
 }
 
 showSlides(slideIndex);
+
+
+// STEP 3: Set interval to automatically change image
+
+// QUESTION: Is it ok to set this variable in the global scope?
+var carouselInterval = setInterval(function() {plusSlides(1);	}, 2000);
+
+
+//When the setInterval() is put in a function clearInterval() does not work.
+// timeout = function (carouselInterval) {
+// 	var carouselInterval = setInterval(function() {plusSlides(1);	}, 5000);
+// } 
+// timeout(); 
+
+
+//STEP 4: Define functions that stop and start interval timer depending if the click count is odd or even
+
+var clickCount = 0;
+clickHandlerPause = function() {
+	document.querySelector('.js-pause').addEventListener('click', function() {
+		var play = document.querySelector('.js-play');
+		var pause = document.querySelector('.js-pause');
+		//alert('We are in clickHandler');
+		clickCount++;
+		if( clickCount%2 == 1) {
+			alert('I am odd');
+			clearInterval(carouselInterval);
+
+			pause.style.display = 'none';
+			play.style.display = 'block';
+		} 
+		console.log(clickCount);
+	})
+} 
+clickHandlerPause();
+
+
+clickHandlerPlay = function() {
+	document.querySelector('.js-play').addEventListener('click', function() {
+		var play = document.querySelector('.js-play');
+		var pause = document.querySelector('.js-pause');
+		//alert('We are in clickHandler');
+		clickCount++;
+		if( clickCount%2 == 0) {
+			alert('I am even');
+			carouselInterval = setInterval(function() {plusSlides(1);	}, 2000);
+
+			play.style.display = 'none';
+			pause.style.display = 'block';
+		} 
+		console.log(clickCount);
+	})
+} 
+clickHandlerPlay();
+
+// var clickCount = 0;
+// clickHandler = function() {
+// 	document.querySelector('.js-pause', '.js-play').addEventListener('click', function() {
+// 		var play = document.querySelector('.js-play');
+// 		var pause = document.querySelector('.js-pause');
+// 		//alert('We are in clickHandler');
+// 		clickCount++;
+// 		if( clickCount%2 == 1) {
+// 			alert('I am odd');
+// 			clearInterval(carouselInterval);
+
+// 			pause.style.display = 'none';
+// 			play.style.display = 'block';
+// 		} else {
+// 			alert('I am even')
+// 			carouselInterval = setInterval(function() {plusSlides(1);	}, 10000);
+
+// 		}
+// 		console.log(clickCount);
+// 	})
+// } 
+// clickHandler();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Pause the setTimeout when the prev or next buttons are clicked
